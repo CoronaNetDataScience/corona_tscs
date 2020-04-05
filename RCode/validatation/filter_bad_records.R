021725eb4dca2930519421ba2d5e1d4ca50dc60d
Luca Messerschmidt <l.messerschmidt@zeppelin-university.net>
2020-04-05 15:15:39 +0200

    bad_record deletion

--- RCode/validatation/filter_bad_records.R
+++ RCode/validatation/filter_bad_records.R
@@ -12,10 +12,9 @@
 # validation var
 # need to correct these, storing info for now
 
+
 qualtrics = mutate(qualtrics,
-                   validation=case_when(ra_name %in% "Laura Mahrenbach"~FALSE,
-                                        # RA Mara Forster asked to start fresh on some entries and thus to ignore the following three entries:
-                                        record_id %in% c(7897317, 5297, 571317)~FALSE,
+                   validation=case_when(
                                         # RA Cheng-Hao Shen had some pretty complicated issues with Palau, that Cindy can resolve
                                         # at a later date, but for now, lets just remove Palau from the data
                                         init_country == 'Palau'~FALSE,
@@ -25,17 +24,29 @@ qualtrics = mutate(qualtrics,
                                         record_id == 933728~FALSE,
                                         # 2890593 ;All American passengers have now disembarked from the Grand Princess.  The following reflects the count as of 8:30 p.m. PDT on March 12: \n \n480 total individuals were disembarked from the ship on March 12, 2020:\n\n    218 individuals were transported via chartered flight to Marine Corps Air Station Miramar near San Diego, California\n    251 individuals were transported via charted flight to Dobbins Air Reserve base near Marietta, Georgia\n    11 individuals were transported for care.
                                         record_id == 2890593~FALSE,
-                                        # these entries should not be possible --- 
-                                        #shouldn't be able to select restriciton of mass gatherings and then type of school
-                                        #shouldn't be able to select quarantine and then type of school
-                                        # shouldn't be able to select quarantine and then type of external restriction
-                                        
-                                        # in terms of the survey version now, I checked on April 2 and this is not possible for this current version of the dataset
-                                        # remove for now, but check what happened later!
-                                        # my guess is that some of these RAs coded these cases as one policy type first, and then backtracked to another  policy type, and qualtrics saved the entries for the sub-types 
-                                        # in the future, we should think about creating code that automates the search for these survey fallacies
-                                        record_id %in% c('3548864', '9939713', '933728', '9523505')~FALSE,
                                         # this entry clearly wrong, remove, validate/fix later
                                         target_who_what_10_TEXT %in% c("Tests","Facilities were meant to increase testing capacity",
                                                                        "Also foreign residents")~FALSE,
                                         TRUE~TRUE))
+
+
+# Log of what has been solved
+
+##ra_name %in% "Laura Mahrenbach"~FALSE,----
+# RA Mara Forster asked to start fresh on some entries and thus to ignore the following three entries:----
+##record_id %in% c(7897317, 5297, 571317)~FALSE,
+## 4979089 and 5608933 Veronika asked us to delete these----
+##record_id  %in% c('4979089','5608933')~FALSE,
+# these entries should not be possible ----
+#shouldn't be able to select restriciton of mass gatherings and then type of school
+#shouldn't be able to select quarantine and then type of school
+# shouldn't be able to select quarantine and then type of external restriction
+# in terms of the survey version now, I checked on April 2 and this is not possible for this current version of the dataset
+# remove for now, but check what happened later!
+# my guess is that some of these RAs coded these cases as one policy type first, and then backtracked to another  policy type, and qualtrics saved the entries for the sub-types 
+# in the future, we should think about creating code that automates the search for these survey fallacies
+##record_id %in% c('3548864', '9939713', '933728', '9523505')~FALSE,
+#----
+test<-coronanet_raw_latest[!is.na(coronanet_raw_latest$target_who_what_10_TEXT),]
+
+test<-test[,c("record_id","target_who_what_10_TEXT")]
