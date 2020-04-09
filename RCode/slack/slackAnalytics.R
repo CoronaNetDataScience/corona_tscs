82b2d1a8269e3a5d219c6012dc1ea4c5352b0293
cindyyawencheng <ccheng623@gmail.com>
2020-04-09 17:31:32 +0200

    add instructions for creating corona_govt_response_slack_latest_clean.rds file

--- RCode/slack/slackAnalytics.R
+++ RCode/slack/slackAnalytics.R
@@ -1,3 +1,5 @@
+if(Sys.info()['user'] == 'cindycheng'){
+pathData = "/Users/cindycheng/Dropbox/corona_tscs/data"}
 
 # load packages
 library(dplyr)
@@ -6,65 +8,62 @@ library(ggplot2)
 library(forcats)
 
 # load data
-pathData = "/Users/cindycheng/Dropbox/corona_tscs/data"
-slack = read.csv(paste0(pathData, "/slack/corona-govt-response_latest.csv"), stringsAsFactors = FALSE)
+slack = read.csv(paste0(pathData, "/slack/corona-govt-response_slack_latest.csv"), stringsAsFactors = FALSE)
 
 
 # subset to March 28, the day data collection started
 slack$Date = as.Date(slack$Date)
 slack =  slack %>% dplyr:::filter(Date >= "2020-03-28")
 
-
 # remove weekly stats
 slack = select(slack, -Weekly.active.members, -Weekly.members.posting.messages)
-slack = slack %>% dplyr:::rename(Members.present.on.Slack = Daily.active.members ,
-							  Members.posting.messages = Daily.members.posting.messages	)
+slack = slack %>% dplyr:::rename(Project.members.present.on.Slack = Daily.active.members ,
+							  Project.members.posting.messages = Daily.members.posting.messages	)
 
 # reshape data to long form
 slack = slack %>% gather("var", "value", -Date)
-
 
 # clean up var names
 slack$var = gsub('\\.', ' ', slack$var)
 
 # set order or vars in factor levels
- 
- 
-slack$var = fct_relevel(slack$var, "Members present on Slack", 
-						"Members posting messages",
-						"Messages posted by members" ,
-						"Messages in DMs"   )
+slack$var = fct_relevel(slack$var, "Project members present on Slack", 
+						"Project members posting messages",
+						"Messages posted by project members" ,
+						"Messages in DMs"  )
 
  
+saveRDS(slack, file = paste0(pathData, '/slack/corona_govt_response_slack_latest_clean.rds'))
 
 
-# make plot of daily active members and daily members posting messges
-
-p1 = ggplot(slack %>% filter(var %in% c("Members present on Slack", "Members posting messages" )),
-			aes(x = Date, y = value, color = var))+
-			geom_line(size = 1)+ 
-			xlab("Date")+
-			ylab("Number of Members")+
-			scale_x_date(date_labels = "%a %d-%m-%y", date_breaks = "day")+
-			scale_color_manual(values=c( "#3B7EA1",  "#FDB515"))+
-			theme_minimal()+
-			theme(legend.position="bottom",
-				  legend.title = element_blank(),
-				  axis.text.x = element_text(angle = 90, hjust = 1),
-				  panel.grid.minor = element_blank())
-
+# # make plot of daily active members and daily members posting messges
+# p1 = ggplot(slack %>% filter(var %in% c("Project members present on Slack", "Project members posting messages" )),
+# 			aes(x = Date, y = value, color = var))+
+# 			geom_line(size = 1)+ 
+# 			xlab("Date")+
+# 			ylab("Number of Project members")+
+# 			scale_x_date(date_labels = "%a %d-%m-%y", date_breaks = "day")+
+# 			scale_color_manual(values=c( "#3B7EA1",  "#FDB515"))+
+# 			theme_minimal()+
+# 			theme(legend.position="bottom",
+# 				  legend.title = element_blank(),
+# 				  axis.text.x = element_text(angle = 90, hjust = 1),
+# 				  panel.grid.minor = element_blank())
 
 
-p2 = ggplot(slack %>% filter(var %in% c( "Messages posted by members", "Messages in DMs" )),
-			aes(x = Date, y = value, color = var))+
-			geom_line(size = 1)+ 
-			xlab("Date")+
-			ylab("Number of Messages")+
-			scale_x_date(date_labels = "%a %d-%m-%y", date_breaks = "day")+
-			scale_color_manual(values=c( "#3B7EA1",  "#FDB515"))+
-			theme_minimal()+
-			theme(legend.position="bottom",
-				  legend.title = element_blank(),
-				  axis.text.x = element_text(angle = 90, hjust = 1),
-				  panel.grid.minor = element_blank())
+# p2 = ggplot(slack %>% 
+# 			filter(var %in% c( "Messages in public channels", "Messages in DMs" )),
+# 			aes(x = Date, y = value, color = var))+
+# 			geom_line(size = 1)+ 
+# 			xlab("Date")+
+# 			ylab("Number of Messages")+
+# 			scale_x_date(date_labels = "%a %d-%m-%y", date_breaks = "day")+
+# 			scale_color_manual(values=c( "#3B7EA1",  "#FDB515"))+
+# 			theme_minimal()+
+# 			theme(legend.position="bottom",
+# 				  legend.title = element_blank(),
+# 				  axis.text.x = element_text(angle = 90, hjust = 1),
+# 				  panel.grid.minor = element_blank())
+ 
+
 
